b53971d57048ba7e57b39d2e2d5de5b50abd8bc8
samitha senewirathna <samithani17@gmail.com>
2025-07-11 15:14:51 +0530

    implement category data

--- backend/ML-service/app/data/idea_titles.py
+++ backend/ML-service/app/data/idea_titles.py
@@ -1,5 +1,18 @@
 idea_data = [
-   
+    {"title": "AI-Powered Study Assistant", "category": "AI"},
+    {"title": "Eco-Friendly Transportation", "category": "Business"},
+    {"title": "Mental Health Tracker", "category": "Healthcare"},
+    {"title": "Blockchain for Voting", "category": "Technology"},
+    {"title": "Future of AI in Education", "category": "AI"},
+    {"title": "Virtual Fitness Trainer", "category": "Entertainment"},
+    {"title": "Food Waste Reduction App", "category": "Business"},
+    {"title": "Intelligent Tutoring System", "category": "AI"},
+    {"title": "Remote Patient Monitoring", "category": "Healthcare"},
+    {"title": "Smart Sports Performance Tracker", "category": "Sports"},
+    {"title": "Business Analytics Dashboard", "category": "Business"},
+    {"title": "AI Coach for Football Strategy", "category": "Sports"},
+    {"title": "Online Streaming Hub", "category": "Entertainment"},
+    {"title": "Tech News Aggregator", "category": "Technology"},
 ]
 
 def get_idea_titles():
